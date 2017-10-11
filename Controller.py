875584d77b0e23118e9dd2a15ca4c6fcd7a019c4
Charlie Vollenweider <Charlie.Vollenweider@gmail.com>
2017-10-11 16:10:23 -0400

    Attempting Threading

--- Controller.py
+++ Controller.py
@@ -1,14 +1,23 @@
 import RPi.GPIO as GPIO
 import time
+import multiprocessing
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 GPIO.setup(18,GPIO.OUT)
+'''
 print("LED on")
 GPIO.output(18,GPIO.HIGH)
 time.sleep(1)
 print("LED off")
 GPIO.output(18,GPIO.LOW)
+'''
+leds = {'red':18,'yellow':23,'green':24,'blue':25}
+redlist = [('on',1),('off',2),('on',1),('off',3),('on',3)]
+bluelist = [('off',1),('on',2),('off',1),('on',3),('off',3)]
+greenlist = [('on',2),('off',2),('on',2),('off',2),('on',2)]
+yellowlist = [('on',1),('off',1),('on',1),('off',1),('on',1),('off',1),('on',1),('off',1),('on',1),('off',1)]
+controllers = [('red',redlist), ('blue',bluelist), ('green',greenlist), ('yellow',yellowlist)]
 
 def lightOn(pin, delay):
     '''Turns on a light at a given pin for a given interval'''
@@ -17,9 +26,19 @@ def lightOn(pin, delay):
     GPIO.output(pin,GPIO.LOW)
     return
 
-alist = [('on',1),('off',2),('on',1),('off',3),('on',4)]
-for each in alist:
-    if each[0] == 'on':
-        lightOn(18,each[1])
-    else:
-        time.sleep(each[1])
+def control(color,controllist):
+    for each in controllist:
+        if each[0] == 'on':
+            lightOn(leds[color],each[1])
+        else:
+            time.sleep(each[1])
+
+if __name__ =='__main__':
+    jobs =[]
+    for each in controllers:
+        p= multiprocessing.Process(target = control, args=each[0],each[1])
+        jobs.append(p)
+        p.start()
+
+
+
