b2a078c24e9fe1b40104f5cdd3cb5d2476a27a2a
Charlie Vollenweider <Charlie.Vollenweider@gmail.com>
2017-10-11 15:46:25 -0400

    Fiddling with lEDs

--- Controller.py
+++ Controller.py
@@ -9,3 +9,17 @@ GPIO.output(18,GPIO.HIGH)
 time.sleep(1)
 print("LED off")
 GPIO.output(18,GPIO.LOW)
+
+def lightOn(pin, delay):
+    '''Turns on a light at a given pin for a given interval'''
+    GPIO.output(pin,GPIO.HIGH)
+    time.sleep(delay)
+    GPIO.output(pin,GPIO.LOW)
+    return
+
+alist = [('on',1),('off',2),('on',1),('off',3),('on',4)]
+for each in alist:
+    if each[0] == 'on':
+        lightOn(18,each[1])
+    else:
+        time.sleep(each[1])
