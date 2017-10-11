2454539fd8f99e349662aec2f6564a938a49811a
Charlie Vollenweider <Charlie.Vollenweider@gmail.com>
2017-10-11 16:16:10 -0400

    Fixing GPIO Setup

--- Controller.py
+++ Controller.py
@@ -2,9 +2,10 @@ import RPi.GPIO as GPIO
 import time
 import multiprocessing
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
-GPIO.setup(18,GPIO.OUT)
+
+
+
+
 '''
 print("LED on")
 GPIO.output(18,GPIO.HIGH)
@@ -12,12 +13,20 @@ time.sleep(1)
 print("LED off")
 GPIO.output(18,GPIO.LOW)
 '''
+
+#Command Data
 leds = {'red':18,'yellow':23,'green':24,'blue':25}
 redlist = [('on',1),('off',2),('on',1),('off',3),('on',3)]
 bluelist = [('off',1),('on',2),('off',1),('on',3),('off',3)]
 greenlist = [('on',2),('off',2),('on',2),('off',2),('on',2)]
 yellowlist = [('on',1),('off',1),('on',1),('off',1),('on',1),('off',1),('on',1),('off',1),('on',1),('off',1)]
 controllers = [('red',redlist), ('blue',bluelist), ('green',greenlist), ('yellow',yellowlist)]
+
+#GPIO SETUP
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+for each in leds.values():
+    GPIO.setup(each, GPIO.OUT)
 
 def lightOn(pin, delay):
     '''Turns on a light at a given pin for a given interval'''
