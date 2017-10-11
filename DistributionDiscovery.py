a93c3872bc67c1b8273565e54754385a84a42492
Charlie Vollenweider <charlie.vollenweider@gmail.com>
2017-10-11 13:35:29 -0400

    Initializing the project
    A Little bit of code I have already

---
+++ DistributionDiscovery.py
@@ -0,0 +1,40 @@
+from kivy.app import App
+from kivy.properties import NumericProperty
+from kivy.uix.boxlayout import BoxLayout
+import datetime
+import pygame
+
+
+class DataEntryApp(BoxLayout):
+    numSamples = NumericProperty(0)
+    #initializationTime = datetime.datetime()
+    #sampleTimes=[initializationTime.now()]
+
+    def newSample(self,*args):
+        self.numSamples +=1
+        #self.sampleTimes.append(datetime.timedelta(self.initializationTime,datetime.now()))
+
+        #print(self.sampleTimes)
+        return
+    def doStuff(self):
+        print("doing things")
+        pygame.init()
+        pygame.mixer.init()
+        pygame.mixer.music.load("KissMeBabe.mp3")
+        pygame.mixer.music.play()
+
+class InitializationApp(App):
+    def build(self):
+        return DataEntryApp()
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    print("running")
+    InitializationApp().run()
