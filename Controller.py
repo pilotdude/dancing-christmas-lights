640dd22a59deddef9e600e423aeb27f6b247e70e
Charlie Vollenweider <Charlie.Vollenweider@gmail.com>
2017-10-11 19:42:59 -0400

    Updating to 7 leds for more testing

--- Controller.py
+++ Controller.py
@@ -1,8 +1,12 @@
 import RPi.GPIO as GPIO
 import time
 import multiprocessing
+import xml.etree.ElementTree as ET
 
+#Midi to xml converter: http://flashmusicgames.com/midi/mid2xml.php
 
+tree = ET.parse('StarWarsTheme.xml')
+root = tree.getroot()
 
 
 
@@ -15,12 +19,16 @@ GPIO.output(18,GPIO.LOW)
 '''
 
 #Command Data
-leds = {'red':18,'yellow':23,'green':24,'blue':25}
-redlist = [('on',1),('off',2),('on',1),('off',3),('on',3)]
-bluelist = [('off',1),('on',2),('off',1),('on',3),('off',3)]
-greenlist = [('on',2),('off',2),('on',2),('off',2),('on',2)]
-yellowlist = [('on',1),('off',1),('on',1),('off',1),('on',1),('off',1),('on',1),('off',1),('on',1),('off',1)]
-controllers = [('red',redlist), ('blue',bluelist), ('green',greenlist), ('yellow',yellowlist)]
+leds = {'red1':18,'yellow1':23,'green1':24,'blue1':25,'red2':16,'green2':20, 'yellow2':12}
+red1list = [('on',1),('off',2),('on',1),('off',3),('on',3)]
+red2list = [('on',.5),('off',1),('on',2),('off',1),('on',3),('off',2),('on',.5)]
+blue1list = [('off',1),('on',2),('off',1),('on',3),('off',3)]
+green1list = [('on',2),('off',2),('on',2),('off',2),('on',2)]
+green2list = [('on',.5),('off',1.5),('on',1),('off',1),('on',.5),('off',1.5),('on',1),('off',1),('on',1.5),('off',.5)]
+yellow1list = [('on',1),('off',1),('on',1),('off',1),('on',1),('off',1),('on',1),('off',1),('on',1),('off',1)]
+yellow2list = [('on',1),('off',1),('on',1),('off',1),('on',.5),('off',1.5),('on',1),('off',1),('on',1.5),('off',.5)]
+controllers = [('red1',red1list), ('blue1',blue1list), ('green1',green1list), ('yellow1',yellow1list),
+               ('red2',red2list),('green2',green2list),('yellow2',yellow2list)]
 
 #GPIO SETUP
 GPIO.setmode(GPIO.BCM)
