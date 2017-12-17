e09d98e8277044efe20428d3e361a04b4bf38657
Charlie Vollenweider <charlie.vollenweider@gmail.com>
2017-12-17 00:09:33 -0500

    Testing

---
+++ testall.py
@@ -0,0 +1,28 @@
+import time
+
+def test():
+    numChannels = 8
+    activePins=['0','1','2','3','4','5','6','7']
+
+    for each in activePins:
+        a=Channels.Channel()
+        a.chanOn()
+    print("all channels on")
+
+    time.sleep(2)
+    for each in activePins:
+        a=Channels.Channel()
+        a.chanOff()
+
+    print("all off")
+
+
+    for each in activePins:
+        a=Channels.Channel()
+        a.setPin(mapDict[each])
+        print(str(each)+"ON")
+        a.chanOnFor(1)
+        print(str(each)+"OFF")
+
+    return
+test()
